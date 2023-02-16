2e42007ac72ca42a1745f47b66c1bb018f293f83
Michael Lesniak <mail@mlesniak.com>
2023-02-16 07:16:52 +0100

    Parse internal logo format and display logo

--- app/src/main/kotlin/com/mlesniak/gameboy/Main.kt
+++ app/src/main/kotlin/com/mlesniak/gameboy/Main.kt
@@ -1,7 +1,6 @@
 package com.mlesniak.gameboy
 
 import com.mlesniak.gameboy.Debug.hexdump
-import java.lang.Byte.parseByte
 import java.nio.file.Files
 import java.nio.file.Path
 
@@ -18,16 +17,54 @@ class Rom(
         hexdump(bytes)
     }
 
-    // 0104-0133 - Nintendo Logo
-    fun dumpLogo() {
-        hexdump(bytes, 0x104..0x0133)
-    }
-
     fun dumpTitle() {
         hexdump(bytes, 0x134..0x143)
     }
 
     fun bytes() = bytes
+
+    // The manufacturer logo is stored in 0x0104..0x0133 in 1bpp format.
+    //
+    // 00000104  ce ed 66 66 cc 0d 00 0b  03 73 00 83 00 0c 00 0d  |..ff.....s......|
+    // 00000114  00 08 11 1f 88 89 00 0e  dc cc 6e e6 dd dd d9 99  |..........n.....|
+    // 00000124  bb bb 67 63 6e 0e ec cc  dd dc 99 9f bb b9 33 3e  |..gcn.........3>|
+    //
+    // The first 24 bytes define rows 0..3, the second 24 bytes rows 4..7 based on the
+    // following schema:
+    //
+    //     C 6 C 0 0 0 0 0 0 1 8 0
+    //     E 6 C 0 3 0 0 0 0 1 8 0
+    //     E 6 0 0 7 8 0 0 0 1 8 0
+    //     D 6 D B 3 3 C D 8 F 9 E
+    //
+    //     D 6 D D B 6 6 E D 9 B 3
+    //     C E D 9 B 7 E C D 9 B 3
+    //     C E D 9 B 6 0 C D 9 B 3
+    //     C 6 D 9 B 3 E C C F 9 E
+    //
+    // Finally, the binary representation of every nimble maps to pixels which are shown.
+    fun showLogo() {
+        val logo = bytes.slice(0x104..0x0133)
+        for (y in 0..7) {
+            val xs = y / 2 + (y / 4 * (logo.size / 2 - 2))
+            val m = if (y > 3) logo.size else logo.size / 2
+            for (x in xs until m step 2) {
+                val nimble: Int =
+                    if (y % 2 == 0) {
+                        logo[x].toInt() / 0x10
+                    } else {
+                        logo[x].toInt() % 0x10
+                    }
+                val pixels = nimble
+                    .toString(2)
+                    .padStart(4, '0')
+                    .replace('1', '█')
+                    .replace('0', ' ')
+                print("$pixels")
+            }
+            println()
+        }
+    }
 
     // private fun tileDecoder() {
     //     // Hack mode on...
@@ -63,4 +100,6 @@ class Rom(
 
 fun main() {
     val rom = Rom("rom/tetris.gb")
+    rom.dumpTitle()
+    rom.showLogo()
 }
