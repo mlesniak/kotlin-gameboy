c4363ad7e4a3ba5f32129fef23f121da2b98af44
Michael Lesniak <mail@mlesniak.com>
2023-02-18 09:20:09 +0100

    Add LD (HL-), A

--- app/src/main/kotlin/com/mlesniak/gameboy/CPU.kt
+++ app/src/main/kotlin/com/mlesniak/gameboy/CPU.kt
@@ -1,5 +1,6 @@
 package com.mlesniak.gameboy
 
+import org.graalvm.collections.EconomicMap
 import java.lang.Integer.max
 import java.lang.Integer.min
 
@@ -10,8 +11,12 @@ class CPU(
     // Should we use bytes or ints?
     var a: Int = 0x00 // Accumulator
     var f: Int = 0x00 // Flags: zero subtraction half-carry carry 0000
+    var hl: Int = 0x00
     var sp: Int = 0x0000 // Stack pointer.
     var pc: Int = 0x0000 // Program counter.
+
+    // Memory including VRAM (0x8000..0x9FFF).
+    val mem: IntArray = IntArray(0x9FFF + 0x01)
 
     // TODO(mlesniak) Refactor once we have a better feeling for the
     //                structure.
@@ -29,10 +34,20 @@ class CPU(
                     a = 0x00
                     f = f or 0b10000000
                 }
+                // LD HL, nnnn
+                0x21 -> {
+                    // Little endian.
+                    hl = nextOpcode() + nextOpcode() * 0x100
+                }
+                // LD (HL-),A
+                0x32 -> {
+                    mem[hl] = a
+                    hl--
+                }
 
                 else -> {
                     dump()
-                    throw IllegalStateException("Unknown opcode ${opcode.hex()} at ${pc.hex()}")
+                    throw IllegalStateException("Unknown opcode ${opcode.hex(2)} at ${pc.hex(4)}")
                 }
             }
         }
@@ -43,6 +58,7 @@ class CPU(
         println("SP ${sp.hex(4)}")
         println("A  ${a.hex(2)}")
         println("F  ${f.binary(8).substring(0..5)}")
+        println("HL ${hl.hex(4)}")
         val ds = (pc - 0x10).clamp(0)
         val es = (pc + 0x10).clamp(0, code.size)
         Debug.hexdump(code, ds..es)
