c94c0765d6df89925443e8dc1174b7eb50a70aa2
Michael Lesniak <mail@mlesniak.com>
2023-03-19 18:28:53 +0100

    Add comment

--- app/src/main/kotlin/com/mlesniak/gameboy/Main.kt
+++ app/src/main/kotlin/com/mlesniak/gameboy/Main.kt
@@ -4,5 +4,6 @@ fun main() {
     val cpu = CPU()
     // TODO(mlesniak) We need to a load rom for the
     //                actual logo data in 0x104.
+    // "Convert and load logo data from cart into Video RAM"
     cpu.boot()
 }
