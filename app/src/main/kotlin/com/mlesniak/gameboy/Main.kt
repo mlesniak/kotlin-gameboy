29fef8f60c2dfeb402f91089261cd3d5cadf70a5
Michael Lesniak <mail@mlesniak.com>
2023-03-26 13:42:40 +0200

    Remove TODO

--- app/src/main/kotlin/com/mlesniak/gameboy/Main.kt
+++ app/src/main/kotlin/com/mlesniak/gameboy/Main.kt
@@ -4,8 +4,5 @@ import java.nio.file.Path
 
 fun main() {
     val cpu = CPU(Path.of("rom/tetris.gb"))
-    // TODO(mlesniak) We need to a load rom for the
-    //                actual logo data in 0x104.
-    // "Convert and load logo data from cart into Video RAM"
     cpu.boot()
 }
